80f52dd9ad29d8ae6b5d99b90e19e4dc080dda8c
Parik <singhparikshit27@live.in>
2020-06-13 04:10:37 +0530

    Subtitle synchronization (requires subtitle data set)

--- src/voices.cpp
+++ src/voices.cpp
@@ -1,16 +1,51 @@
 #include "voices.h"
+#include <cstdint>
+#include <unordered_map>
+
+struct MissionAudioData
+{
+	char* SoundName;
+	uint32_t SoundId;
+};
+
+std::unordered_map<std::string, std::string> subtitles{{
+		{"lib_a1", "ebal_a"}
+	}};
+
+MissionAudioData* MissionAudioNameSfxAssoc = (MissionAudioData*) 0x607094;
+
+const int STREAMED_SOUND_MISSION_LIB_A1 = 0x5F;
+const int STREAMED_SOUND_MISSION_C1 = 0xC3;
 
 static int GetRandomizedMissionAudioSfx (const char* name)
 {
-	const int STREAMED_SOUND_MISSION_LIB_A1 = 0x5F;
-	const int STREAMED_SOUND_MISSION_C1 = 0xC3;
 
-	return RandomNumber(STREAMED_SOUND_MISSION_LIB_A1,
-			    STREAMED_SOUND_MISSION_C1);
+	int sound = RandomNumber(STREAMED_SOUND_MISSION_LIB_A1,
+				 STREAMED_SOUND_MISSION_C1);
+
+	char* newName = MissionAudioNameSfxAssoc
+		[sound - STREAMED_SOUND_MISSION_LIBA1].SoundName;
+
+	voiceLines[sound] = newName;
+	if (subtitles.count(newName))
+		voiceLines[sound] = subtitles[newName];
+	
+	return sound;
+}
+
+char* __fastcall voices::FixSubtitles(CText* text, void* edx, char* key)
+{
+	if (voices.count(key))
+		key = voiceLines[key];
+	
+	return plugin::CallMethodAndReturn<char*, 0x52C5A0, CText *>(text, key);
 }
 
 void voices::Initialise()
 {
 	if (Config::VoiceLineRandomizer::Enabled)
-		plugin::patch::RedirectCall(0x57955D, GetRandomizedMissionAudioSfx);
+		{
+			plugin::patch::RedirectCall(0x57955D, GetRandomizedMissionAudioSfx);
+			plugin::patch::RedirectCall(0x43D119, voices::FixSubtitles);
+		}
 }
--- src/voices.h
+++ src/voices.h
@@ -2,10 +2,15 @@
 #include "plugin.h"
 #include "Config.h"
 #include "Functions.h"
+#include <string>
+#include <unordered_map>
+
+class CText;
 
 class voices : Functions
 {
-	static int GetRandomizedMissionAudioSfx (const char* name);
+	static std::unordered_map<std::string, std::string> voiceLines;
+	static char* __fastcall FixSubtitles(CText* t, void* edx, char* key);
 	
 public:
 	static void Initialise();
